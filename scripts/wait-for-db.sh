8d08025658c30fc1d11631a44aee64d5d5948fff
Manuel Rivera Gálvez <rivgal2005@gmail.com>
2026-09-21 17:31:51 +0200

    fis ssl

--- scripts/wait-for-db.sh
+++ scripts/wait-for-db.sh
@@ -15,7 +15,7 @@
 echo "Starting wait-for-db.sh"
 echo "Hostname: $MARIADB_HOSTNAME, Port: $MARIADB_PORT, User: $MARIADB_USER"
 
-while ! mariadb -h "$MARIADB_HOSTNAME" -P "$MARIADB_PORT" -u"$MARIADB_USER" -p"$MARIADB_PASSWORD" -e 'SELECT 1'; do
+while ! mariadb --skip-ssl -h "$MARIADB_HOSTNAME" -P "$MARIADB_PORT" -u"$MARIADB_USER" -p"$MARIADB_PASSWORD" -e 'SELECT 1'; do
   echo "MariaDB is unavailable - sleeping"
   sleep 1
 done
